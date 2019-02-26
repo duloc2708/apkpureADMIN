252c2dffefc80bad4f7ccffa2cc0b10f52444650
Loc Du <locdu@kolabs.co>
2019-02-26 10:43:12 +0700

    update code change id slot game

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -1,8 +1,8 @@
 export const DEFAULT_FOLDER = '/';
 export const DEFAULT_URL = 'http://localhost:1337';
 
-// export const API_URL = 'http://localhost:1337/api/' /*dev*/
-export const API_URL = 'http://api.apksafety.com/api/' /*prod*/
+export const API_URL = 'http://localhost:1337/api/' /*dev*/
+// export const API_URL = 'http://api.apksafety.com/api/' /*prod*/
 
 
 export const DEFAULT_DATE = '1900-01-01 00:00:00';
