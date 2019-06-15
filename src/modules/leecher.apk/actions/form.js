efedd08eb107d539d493d08e8f405674cdc5e24e
Loc Du <locdu@kolabs.co>
2019-06-15 19:40:55 +0700

    update

--- src/modules/leecher.apk/actions/form.js
+++ src/modules/leecher.apk/actions/form.js
@@ -33,13 +33,30 @@ export const getListGame = () => {
         let { url } = getState().leecher
         let id = url
         return new Promise((resolve, reject) => {
-            axios.post(`${Config.API_URL}getapk`, { id: id })
-                .then((response) => {
-                    // let { Data } = response.data
-                    resolve(response)
-                }, (err) => {
-                    reject(err)
+            axios.post(`http://61.28.230.226:3001/api/test/getlink`, { id: id })
+                .then(function (response) {
+                    let { data } = response.data
+                    axios({
+                        method: 'post',
+                        url: `https://api-apk.evozi.com/download`,
+                        data: data,
+                        config: { headers: { 'Content-Type': 'multipart/form-data' } }
+                    })
+                        .then(function (response2) {
+                            //handle success
+                            resolve(response2)
+                            console.log(response2);
+                        })
+                        .catch(function (response2) {
+                            //handle error
+                            reject(response2)
+                            console.log(response2);
+                        });
                 })
+                .catch(function (response) {
+                    //handle error
+                    console.log(response);
+                });
         })
     }
 }
